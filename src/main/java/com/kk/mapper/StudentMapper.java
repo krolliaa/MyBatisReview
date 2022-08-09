3f044870b22706def458c328b2897db3ac9411f2
krolliaa <krolliaa@163.com>
2022-08-09 11:46:45 +0800

    MyBatis 使用 Map 引入多个参数

--- src/main/java/com/kk/mapper/StudentMapper.java
+++ src/main/java/com/kk/mapper/StudentMapper.java
@@ -2,10 +2,13 @@ package com.kk.mapper;
 
 import com.kk.pojo.Student;
 
+import java.util.Map;
+
 public interface StudentMapper {
     public abstract int insertStudent();
     public abstract int deleteStudent();
     public abstract int updateStudent();
     public abstract Student selectStudent();
     public abstract Student selectStudentByName(String name);
+    public abstract Student selectStudentByNameAndAge(Map map);
 }
--- src/main/java/com/kk/service/StudentService2.java
+++ src/main/java/com/kk/service/StudentService2.java
@@ -6,6 +6,8 @@ import com.kk.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentService2 {
     public static void main(String[] args) throws IOException {
@@ -13,5 +15,10 @@ public class StudentService2 {
         StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
         Student student = studentMapper.selectStudentByName("tom");
         System.out.println(student);
+        Map map = new HashMap<String, String>();
+        map.put("name", "tom");
+        map.put("age", "13");
+        Student student1 = studentMapper.selectStudentByNameAndAge(map);
+        System.out.println(student1);
     }
 }
