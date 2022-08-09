2346423dc4c6fca8c966a2615ada9b1bb3963826
krolliaa <krolliaa@163.com>
2022-08-09 11:10:56 +0800

    MyBatis 实现增删改查

--- src/main/java/com/kk/mapper/StudentMapper.java
+++ src/main/java/com/kk/mapper/StudentMapper.java
@@ -1,5 +1,10 @@
 package com.kk.mapper;
 
+import com.kk.pojo.Student;
+
 public interface StudentMapper {
     public abstract int insertStudent();
+    public abstract int deleteStudent();
+    public abstract int updateStudent();
+    public abstract Student selectStudent();
 }
--- src/main/java/com/kk/service/StudentService.java
+++ src/main/java/com/kk/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.kk.service;
 
 import com.kk.mapper.StudentMapper;
+import com.kk.pojo.Student;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -16,7 +17,15 @@ public class StudentService {
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
         StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
-        int result = studentMapper.insertStudent();
-        System.out.println(result);
+        int resultInsert = studentMapper.insertStudent();
+        System.out.println("resultInsert = " + resultInsert);
+        Student resultSelect1 = studentMapper.selectStudent();
+        System.out.println("resultSelect1 = " + resultSelect1);
+        int resultUpdate = studentMapper.updateStudent();
+        System.out.println("resultUpdate = " + resultUpdate);
+        Student resultSelect2 = studentMapper.selectStudent();
+        System.out.println("resultSelect2 = " + resultSelect2);
+        int resultDelete = studentMapper.deleteStudent();
+        System.out.println("resultDelete = " + resultDelete);
     }
 }
