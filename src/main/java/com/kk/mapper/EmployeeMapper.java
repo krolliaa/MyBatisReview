2863fb36e8f663606a5caf22b0b58ad726554ce3
krolliaa <krolliaa@163.com>
2022-08-09 14:44:01 +0800

    MyBatis ResultMap 解决实体类属性名跟数据库字段名不一致问题

--- src/main/java/com/kk/mapper/EmployeeMapper.java
+++ src/main/java/com/kk/mapper/EmployeeMapper.java
@@ -1,10 +1,12 @@
 package com.kk.mapper;
 
 import com.kk.pojo.Employee;
+import org.omg.CORBA.portable.ValueBase;
 
 import java.util.List;
 
 public interface EmployeeMapper {
     public abstract List<Employee> selectEmployeeByListTypeAliases();
-    public abstract List<Employee> selectEmployeeByListMapUnderscorpeToCamelCase();
+    public abstract List<Employee> selectEmployeeByListMapUnderscoreToCamelCase();
+    public abstract List<Employee> selectEmployeeByResultMap();
 }
--- src/main/java/com/kk/service/StudentService5.java
+++ src/main/java/com/kk/service/StudentService5.java
@@ -1,8 +1,6 @@
 package com.kk.service;
 
 import com.kk.mapper.EmployeeMapper;
-import com.kk.mapper.StudentMapper;
-import com.kk.pojo.Student;
 import com.kk.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
@@ -14,7 +12,9 @@ public class StudentService5 {
         EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
         List list1 = employeeMapper.selectEmployeeByListTypeAliases();
         System.out.println(list1);
-        List list2 = employeeMapper.selectEmployeeByListMapUnderscorpeToCamelCase();
+        List list2 = employeeMapper.selectEmployeeByListMapUnderscoreToCamelCase();
         System.out.println(list2);
+        List list3 = employeeMapper.selectEmployeeByResultMap();
+        System.out.println(list3);
     }
 }
