ead57395b79567231177ae02c645b7905ca3d5e2
krolliaa <krolliaa@163.com>
2022-08-09 16:46:04 +0800

    二级缓存

--- src/main/java/com/kk/service/StudentService10.java
+++ src/main/java/com/kk/service/StudentService10.java
@@ -19,6 +19,15 @@ public class StudentService10 {
             3.SqlSession 和 SQL 语句虽然都相同但是两个执行中间有增删改操作
             4.SqlSession 和 SQL 语句虽然都相同但是手动清除了缓存造成一级缓存失效
          */
+
+        /*
+            二级缓存：一级缓存默认开启，二级缓存默认不开启
+            使用二级缓存需要满足四个条件：
+            1.MyBatis 配置文件中 <settings> 标签中需要使用 <setting name="cacheEnabled" value="true"/>
+            2.在 mapper.xml 相关 SQL 语句中使用 <cache/> 标签
+            3.二级缓存必须在 SqlSession 关闭或提交之后有效，否则数据将保存在一级缓存中，只有在关闭/提交了之后才会保存到二级缓存中
+            4.实体类需要使用学历恶化 implements Serializable
+         */
         List<Department> departmentList1 = departmentMapper.selectDepartmentByDId(1);
         System.out.println(departmentList1);
         List<Department> departmentList2 = departmentMapper.selectDepartmentByDId(1);
